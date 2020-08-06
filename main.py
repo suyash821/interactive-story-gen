4d5a563b11e45a5dbb404486a0f67ae08c13bec5
suyash821 <41555635+suyash821@users.noreply.github.com>
2020-08-06 18:00:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#Made by @Suyash for 'Tech with Tim Code Jam'
+#Enjoy reading through the script
+
+import time
+import sys
+
+print('Welcome to the interactive Story generator')
+ini_text = 'Starting'
+
+sys.stdout.write(ini_text)
+sys.stdout.flush()
+
+for some in range(0,4):
+  sys.stdout.write('.')
+  sys.stdout.flush()
+  time.sleep(1)
+
+print('\n\n\nBefore continuing, let me explain you somethings')
+some_Random_Things_Which_I_Dont_Want_Later = input('Hit (enter) for the explanation:')
+
+print('\n\nOk, so this program generates a story with you. \nAnd yes, your involvement is very necessary, because this is the so called, \'Interactive Story Generator\'. \nThus it generates a story based on your story, which sounds nonsensical, and.....\n\nYes, it is, just like the program. It\'ll make more sense as I explain you this')
+some_Random_Things_Which_I_Dont_Want_Later = input('\nThere\'s no escape from this.If I\'ve spent so long in typing this out, you have to read it!!\n Hit (enter) for more')
+
+print('\n\nOk, to the actual explanation\n You type out a sentence, and based on it, the program will type another.\n Just keep on repeating it until you\'re okay with it. \nIf you think it\'s enough, just type out \'!quit\' without quotes to end the story.\n\n\nAnd hey, you expect the story to make sense? Test it yourself to see. \nOnto the story generation')
+some_Random_Things_Which_I_Dont_Want_Later = input('Hit (enter) to begin your journey:')
